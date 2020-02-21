0aa6104f32898642da54a6a59bd4b7c4c6a1a1cb
majd ewawi <majd199ziad@gmail.com>
2020-02-21 19:31:26 +0200

    solve an exception in the server code

--- src/serversocket/Server.java
+++ src/serversocket/Server.java
@@ -17,64 +17,65 @@ import java.util.StringTokenizer;
  * @author majd1
  */
 public class Server {
-        public static void main(String args[]) throws IOException
-    { 
-  
+
+    public static void main(String args[]) throws IOException {
+
         // Establish the socket connection. 
-        ServerSocket ss = new ServerSocket(3006); 
+        ServerSocket ss = new ServerSocket(3006);
         System.out.println("Server is ready for connections: ");
-        Socket s = ss.accept(); 
-        
+        Socket s = ss.accept();
+
         System.out.println("There is a client");
-  
+        System.out.println("----------------------------------------");
+
         //  Processing the request. 
-        DataInputStream dis = new DataInputStream(s.getInputStream()); 
-        DataOutputStream dos = new DataOutputStream(s.getOutputStream()); 
-  
-        while (true) 
-        { 
-            // wait for input 
-            String input = dis.readUTF(); 
-  
-            if(input.equals("bye")) 
-                break; 
-  
-            System.out.println("Equation received:-" + input); 
-            int result; 
-  
-            // Use StringTokenizer to break the equation into operand and 
-            // operation 
-            StringTokenizer st = new StringTokenizer(input); 
-  
-            int number1 = Integer.parseInt(st.nextToken()); 
-            String operation = st.nextToken(); 
-            int number2 = Integer.parseInt(st.nextToken()); 
-  
-            // perform the required operation. 
-            if (operation.equals("+")) 
-            { 
-                result = number1 + number2; 
-            } 
-  
-            else if (operation.equals("-")) 
-            { 
-                result = number1 - number2; 
-            } 
-            else if (operation.equals("*")) 
-            { 
-                result = number1 * number2; 
-            } 
-            else
-            { 
-                result = number1 / number2; 
-            } 
-            System.out.println("Client Entered :\nfirst number: "+number1);
-            System.out.println("Second number: "+number2);
-            System.out.println("Result: "+ result);
-            System.out.println("Sending the result to the  client..."); 
-  
-            // send the result back to the client. 
-            dos.writeUTF(Integer.toString(result)); 
-        } 
-    } 
+        DataInputStream dis = new DataInputStream(s.getInputStream());
+        DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+        try {
+            while (true) {
+                // wait for input 
+                String input = dis.readUTF();
+
+                if (input.equals("exit")) {
+                    break;
+                }
+
+                System.out.println("Equation received: " + input);
+                double result;
+
+                // Use StringTokenizer to break the equation into operand and 
+                // operation 
+                StringTokenizer st = new StringTokenizer(input);
+
+                double number1 = Integer.parseInt(st.nextToken());
+                String operation = st.nextToken();
+                double number2 = Integer.parseInt(st.nextToken());
+
+                // perform the required operation. 
+                if (operation.equals("+")) {
+                    result = number1 + number2;
+                } else if (operation.equals("-")) {
+                    result = number1 - number2;
+                } else if (operation.equals("*")) {
+                    result = number1 * number2;
+                } else {
+                    result = number1 / number2;
+                }
+                System.out.println("Client Entered :\nfirst number: " + number1);
+                System.out.println("Second number: " + number2);
+                System.out.println("Result: " + result);
+                System.out.println("Sending the result to the  client...");
+                System.out.println("----------------------------------------");
+
+                // send the result back to the client. 
+                dos.writeUTF(Double.toString(result));
+            }
+
+        } catch (Exception ex) {
+            System.out.println("done");
+        } finally {
+            s.close();
+            ss.close();
+        }
+    }
 }
